922765fcd72b473b51dafec4898113f9af8a20b8
LoganKalib <126275811+LoganKalib@users.noreply.github.com>
2023-11-13 15:04:22 +0200

    progress
    coded the first core concept section. also restructured the files to separate component files

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,21 +1,39 @@
-function Header() {
-  return (
-    <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        Fundamental React concepts you will need for almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
+import compImg from "./assets/components.png";
+import Header from "./components/Header";
+import CoreConcept from "./components/CoreConcepts";
+
+// we import the array that is defined in data file
+import { CORE_CONCEPTS } from "./data";
 
 function App() {
   return (
     <div>
-      // we can call components that render HTML within other components, using the below syntax
+      {/*we can call components that render HTML within other components, using the below syntax */}
       <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          {/*in this section we use the component we created above, and define props/properties. 
+          these custom properties are given values that is used by the component, to create the list items*/}
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The core UI building block"
+              image={compImg}
+            />
+            {/* once imported, we can also then use the core_concepts to populate our props*/}
+            <CoreConcept
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}
+            />
+            {/* if the objects key value pairs is the same as our props we can use a shorthand,
+            to populate them*/}
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+
         <h2>Time to get started!</h2>
       </main>
     </div>
---
+++ 01-starting-project/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import reactImg from "../assets/react-core-concepts.png";
+
+const descriptions = ["Fundamentals", "Crucial", "core"];
+const getItem = (max) => Math.floor(Math.random() * (max + 1));
+
+export default function Header() {
+    return (
+      <header>
+        {/* using src="src/assets/react-core-concepts.png" can cause issues when deploying, as the image could get lost.
+        a better method would be to import the image as see above.*/}
+        <img src={reactImg} alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {/*the below is how we make dynamic content using the const created above*/}
+          {descriptions[getItem(2)]} React concepts you will need for almost any
+          app you are going to build!
+        </p>
+      </header>
+    );
+  }
