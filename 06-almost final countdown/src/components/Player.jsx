7c8fecce47b1693494269ec510fe834b2c537491
LoganKalib <126275811+LoganKalib@users.noreply.github.com>
2023-11-21 10:33:45 +0200

    name updater complete

--- 06-almost final countdown/src/components/Player.jsx
+++ 06-almost final countdown/src/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const playerInput = useRef();
+  const [playerName, setPlayerName] = useState(null);
+
+  function handleClick() {
+    setPlayerName(playerInput.current.value);
+    playerInput.current.value = "";
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerInput} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
---
+++ 06-almost final countdown/src/components/TimerChallenge.jsx
@@ -0,0 +1,7 @@
+export default function TimerChallenge() {
+  return (
+    <section className="challenge">
+      <h2></h2>
+    </section>
+  );
+}
