cec6397ed4ba6b3d56f521341dcb03effef09f66
LoganKalib <126275811+LoganKalib@users.noreply.github.com>
2023-12-06 11:11:19 +0200

    first components

---
+++ 10-Quiz-app/src/Components/Quiz.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+function Quiz() {
+  const [activeQuestion, setActiveQuestion] = useState(0);
+  const [userAnswer, setUserAnswer] = useState([]);
+
+  return;
+}
+
+export default Quiz;
